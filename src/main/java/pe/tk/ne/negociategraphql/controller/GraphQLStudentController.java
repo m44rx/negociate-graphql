a13dedc11f71631c1568e3e3b73feeb0316ff5eb
Eddie Mallma Alvarez <emallmaa@proovedor.intercorp.com.pe>
2023-05-24 16:40:58 -0500

    unused import remove

--- src/main/java/pe/tk/ne/negociategraphql/controller/GraphQLStudentController.java
+++ src/main/java/pe/tk/ne/negociategraphql/controller/GraphQLStudentController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
